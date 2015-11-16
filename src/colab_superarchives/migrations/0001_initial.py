bf66a0bd662f58cec4ecb133eb7c1b5a6e559b6a
Gust <darksshades@hotmail.com>
2015-11-16 15:54:01 -0200

    Add correct migrations
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>

--- src/colab_superarchives/migrations/0001_initial.py
+++ src/colab_superarchives/migrations/0001_initial.py
@@ -3,62 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import hitcounter.models
-import taggit.managers
-import django.db.models.deletion
 from django.conf import settings
-import colab.super_archives.models
+import taggit.managers
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('taggit', '0001_initial'),
+        ('taggit', '0002_auto_20141211_1714'),
+        ('accounts', '0007_auto_20151105_0120'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='EmailAddress',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
-                ('address', models.EmailField(unique=True, max_length=75)),
-                ('real_name', models.CharField(db_index=True, max_length=64,
-                                               blank=True)),
-                ('md5', models.CharField(max_length=32, null=True)),
-                ('user', models.ForeignKey(related_name=b'emails',
-                 on_delete=django.db.models.deletion.SET_NULL,
-                 to=settings.AUTH_USER_MODEL, null=True)),
-            ],
-            options={
-                'ordering': ('id',),
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
-            name='EmailAddressValidation',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
-                ('address', models.EmailField(unique=True, max_length=75)),
-                ('validation_key',
-                 models.CharField(
-                     default=colab.super_archives.models.get_validation_key,
-                     max_length=32, null=True)),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('user',
-                 models.ForeignKey(related_name=b'emails_not_validated',
-                                   to=settings.AUTH_USER_MODEL, null=True)),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
             name='Keyword',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('keyword', models.CharField(max_length=b'128')),
                 ('weight', models.IntegerField(default=0)),
             ],
@@ -70,13 +31,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MailingList',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=80)),
                 ('email', models.EmailField(max_length=75)),
                 ('description', models.TextField()),
                 ('logo', models.FileField(upload_to=b'list_logo')),
                 ('last_imported_index', models.IntegerField(default=0)),
+                ('is_private', models.BooleanField(default=False)),
             ],
             options={
             },
@@ -85,10 +46,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MailingListMembership',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
-                ('mailinglist',
-                 models.ForeignKey(to='super_archives.MailingList')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('mailinglist', models.ForeignKey(to='colab_superarchives.MailingList')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
@@ -98,23 +57,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Message',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
-                ('subject',
-                 models.CharField(help_text='Please enter a message subject',
-                                  max_length=512, verbose_name='Subject',
-                                  db_index=True)),
-                ('subject_clean', models.CharField(max_length=512,
-                                                   db_index=True)),
-                ('body',
-                 models.TextField(default=b'',
-                                  help_text='Please enter a message body',
-                                  verbose_name='Message body')),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('subject', models.CharField(help_text='Please enter a message subject', max_length=512, verbose_name='Subject', db_index=True)),
+                ('subject_clean', models.CharField(max_length=512, db_index=True)),
+                ('body', models.TextField(default=b'', help_text='Please enter a message body', verbose_name='Message body')),
                 ('received_time', models.DateTimeField(db_index=True)),
                 ('message_id', models.CharField(max_length=512)),
                 ('spam', models.BooleanField(default=False)),
-                ('from_address',
-                 models.ForeignKey(to='super_archives.EmailAddress')),
+                ('from_address', models.ForeignKey(to='accounts.EmailAddress')),
             ],
             options={
                 'ordering': ('received_time',),
@@ -126,13 +76,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MessageBlock',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('text', models.TextField()),
                 ('is_reply', models.BooleanField(default=False)),
                 ('order', models.IntegerField()),
-                ('message', models.ForeignKey(related_name=b'blocks',
-                                              to='super_archives.Message')),
+                ('message', models.ForeignKey(related_name='blocks', to='colab_superarchives.Message')),
             ],
             options={
                 'ordering': ('order',),
@@ -142,11 +90,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='MessageMetadata',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=512)),
                 ('value', models.TextField()),
-                ('Message', models.ForeignKey(to='super_archives.Message')),
+                ('Message', models.ForeignKey(to='colab_superarchives.Message')),
             ],
             options={
             },
@@ -155,29 +102,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Thread',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('subject_token', models.CharField(max_length=512)),
-                ('score', models.IntegerField(default=0,
-                                              help_text='Thread score',
-                                              verbose_name='Score')),
+                ('score', models.IntegerField(default=0, help_text='Thread score', verbose_name='Score')),
                 ('spam', models.BooleanField(default=False)),
-                ('latest_message',
-                 models.OneToOneField(related_name=b'+',
-                                      null=True, to='super_archives.Message',
-                                      help_text='Latest message posted',
-                                      verbose_name='Latest message')),
-                ('mailinglist',
-                 models.ForeignKey(
-                     verbose_name='Mailing List',
-                     to='super_archives.MailingList',
-                     help_text='The Mailing List where is the thread')),
-                ('tags',
-                 taggit.managers.TaggableManager(
-                     to='taggit.Tag',
-                     through='taggit.TaggedItem',
-                     help_text='A comma-separated list of tags.',
-                     verbose_name='Tags')),
+                ('latest_message', models.OneToOneField(related_name='+', null=True, to='colab_superarchives.Message', help_text='Latest message posted', verbose_name='Latest message')),
+                ('mailinglist', models.ForeignKey(verbose_name='Mailing List', to='colab_superarchives.MailingList', help_text='The Mailing List where is the thread')),
+                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
             ],
             options={
                 'ordering': ('-latest_message__received_time',),
@@ -189,10 +120,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Vote',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False,
-                                        auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
-                ('message', models.ForeignKey(to='super_archives.Message')),
+                ('message', models.ForeignKey(to='colab_superarchives.Message')),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
@@ -210,7 +140,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='message',
             name='thread',
-            field=models.ForeignKey(to='super_archives.Thread', null=True),
+            field=models.ForeignKey(to='colab_superarchives.Thread', null=True),
             preserve_default=True,
         ),
         migrations.AlterUniqueTogether(
@@ -220,11 +150,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='keyword',
             name='thread',
-            field=models.ForeignKey(to='super_archives.Thread'),
+            field=models.ForeignKey(to='colab_superarchives.Thread'),
             preserve_default=True,
-        ),
-        migrations.AlterUniqueTogether(
-            name='emailaddressvalidation',
-            unique_together=set([('user', 'address')]),
         ),
     ]
