1d860786a582f6de4c71bd36dcaf67a81bb00adf
Charles Oliveira <18oliveira.charles@gmail.com>
2015-12-03 20:52:52 -0500

    Add namespace

--- src/colab_superarchives/apps.py
+++ src/colab_superarchives/apps.py
@@ -5,3 +5,4 @@ from colab.plugins.utils.apps import ColabPluginAppConfig
 class SuperArchivesConfig(ColabPluginAppConfig):
     name = 'colab_superarchives'
     verbose_name = 'Super Archives'
+    namespace = 'archives'
