f30f2890f9922f88d1b4e78a63dece6af3863ece
Alexandre Barbosa <alexandreab@live.com>
2016-01-14 18:00:57 -0200

    Fix imparcial tests
    Signed-off-by: Alexandre Barbosa <alexandreab@live.com>
    Signed-off-by: Gustavo Jaruga <darksshades@gmail.com>

--- tests/test_superarchives_search_view.py
+++ tests/test_superarchives_search_view.py
@@ -10,14 +10,14 @@ from colab.search.forms import ColabSearchForm
 
 class SuperarchivesSearchViewTest(TestCase):
 
-    fixtures = ['test_data.json']
+    fixtures = ['test_data_superachives.json']
 
     def setUp(self):
-        call_command('rebuild_index', interactive=False, verbosity=0)
+        call_command('rebuild_index', interactive=False, verbosity=1)
         self.client = Client()
 
     def tearDown(self):
-        call_command('clear_index', interactive=False, verbosity=0)
+        call_command('clear_index', interactive=False, verbosity=1)
 
     def test_search_thread(self):
         request = self.client.get('/search/?q=thread')
@@ -38,14 +38,15 @@ class SuperarchivesSearchViewTest(TestCase):
     def test_search_multiple_filters(self):
         request = self.client.get('/search/?q=&type=thread+user')
         user_list = request.context['page'].object_list
+        user_list.sort(key=lambda i : i.name)
 
         self.assertEqual(6, len(user_list))
 
-        self.assertIn('Admin Administrator',  user_list[3].author)
-        self.assertIn('Response to Thread 1A',  user_list[3].title)
+        self.assertIn('Admin Administrator',  user_list[0].author)
+        self.assertIn('Response to Thread 1A',  user_list[0].title)
 
-        self.assertIn('Admin Administrator',  user_list[4].author)
-        self.assertIn('Message 1 on Thread 1B',  user_list[4].title)
+        self.assertIn('Admin Administrator',  user_list[1].author)
+        self.assertIn('Message 1 on Thread 1B',  user_list[1].title)
 
-        self.assertIn('Admin Administrator',  user_list[5].author)
-        self.assertIn('Message 1 on Thread 1C',  user_list[5].title)
+        self.assertIn('Admin Administrator',  user_list[2].author)
+        self.assertIn('Message 1 on Thread 1C',  user_list[2].title)
